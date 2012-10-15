15d826180d524848f391d61b92f3fc560d74e3b7
Nixforest21991920@gmail.com <Nixforest21991920@gmail.com@72819721-2bc9-f2f2-fa2e-d6c76ee130ff>
2012-10-15 14:44:33 +0000

    Lưu trữ các biến toàn cục, các string tĩnh.

---
+++ SmartConsumer/src/com/gae/java/smartconsumer/util/GlobalVariable.java
@@ -0,0 +1,21 @@
+/**
+ * GlobalVariable.java
+ * 14 Oct 2012
+ * SmartConsumer.
+ */
+package com.gae.java.smartconsumer.util;
+
+/**
+ * Global variable.
+ * @version v1.0 - 14/10/2012 - NguyenPT
+ * @author NguyenPT
+ */
+public final class GlobalVariable {
+    public static final int CITY_HANOI = 22;
+    public static final int CITY_TPHCM = 29;
+    public static final int CITY_DANANG = 15;
+    public static final int CITY_NHATRANG = 68;
+    public static final int CITY_HAIPHONG = 26;
+    public static final int CITY_VUNGTAU = 67;
+    public static final int CITY_CANTHO = 14;
+}
