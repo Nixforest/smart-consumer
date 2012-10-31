2d5dd49832f41057185d2a58ae90568dc0b05e0c
Nixforest21991920@gmail.com <Nixforest21991920@gmail.com@72819721-2bc9-f2f2-fa2e-d6c76ee130ff>
2012-10-31 16:20:20 +0000

    Update comment

--- SmartConsumer/src/com/gae/java/smartconsumer/servlet/AutoCollectorServlet.java
+++ SmartConsumer/src/com/gae/java/smartconsumer/servlet/AutoCollectorServlet.java
@@ -1,14 +1,11 @@
 /**
  * AutoCollectorServlet.java
- * 
- * 28/5/2012
- * 
+ * 28/05/2012
  * Smart Consumer project
  */
 package com.gae.java.smartconsumer.servlet;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,33 +15,29 @@ import javax.servlet.http.HttpServletResponse;
 import com.gae.java.smartconsumer.util.GetDealFunction;
 
 /**
- * Controller autocollector
- * 
+ * Controller autocollector.
  * @version 1.0 28/5/2012
- * @author Nixforest
+ * @author NguyenPT
  */
 public class AutoCollectorServlet extends HttpServlet {
-    private static final Logger log = Logger.getLogger(AutoCollectorServlet.class.getName());
+    /**  . */
+    private static final long serialVersionUID = 1L;
+    /**
+     * Do Get.
+     * @param req Request
+     * @param resp Response
+     * @throws IOException Exception
+     * @throws ServletException ServerletException
+     * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest,
+     * javax.servlet.http.HttpServletResponse)
+     */
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
         throws IOException, ServletException {
-        log.info("Start AutoCollectorServlet");
         try {
-            // 123do
-            log.info("Start collect from http://123do.vn/");
-            GetDealFunction.getFrom123doVn("http://123do.vn/", 0);
-            GetDealFunction.getFrom123doVn("http://123do.vn/dealhot.php", 1);
-            
-            log.info("Start collect from http://muachung.vn/");
-            GetDealFunction.getFromMuaChungVn();
-
-            log.info("Start collect from http://www.hotdeal.vn/");
-            GetDealFunction.getFromHotDealVn("http://www.hotdeal.vn/ho-chi-minh/");
-            GetDealFunction.getFromHotDealVn("http://www.hotdeal.vn/ho-chi-minh/?page=2");
-            GetDealFunction.getFromHotDealVn("http://www.hotdeal.vn/ha-noi/");
-            GetDealFunction.getFromHotDealVn("http://www.hotdeal.vn/ha-noi/?page=2");
-            GetDealFunction.getFromHotDealVn("http://www.hotdeal.vn/ha-noi/?page=3");
+            GetDealFunction.getFromMuaChungVnNew();
         } catch (Exception ex) {
-            log.severe(ex.getMessage());
+            // TODO Auto-generated catch block
+            ex.printStackTrace();
         }
         resp.sendRedirect("/smartconsumer");
     }
