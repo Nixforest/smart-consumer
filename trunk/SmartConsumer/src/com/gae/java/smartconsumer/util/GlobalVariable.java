288551f3145c1b44b221b2c32ac496bf8e2eaa54
Nixforest21991920@gmail.com <Nixforest21991920@gmail.com@72819721-2bc9-f2f2-fa2e-d6c76ee130ff>
2012-10-31 16:52:38 +0000

    Update comment

--- trunk/SmartConsumer/src/com/gae/java/smartconsumer/util/GlobalVariable.java
+++ trunk/SmartConsumer/src/com/gae/java/smartconsumer/util/GlobalVariable.java
@@ -11,13 +11,45 @@ package com.gae.java.smartconsumer.util;
  * @author NguyenPT
  */
 public final class GlobalVariable {
+    /**
+     * Constructor.
+     */
+    private GlobalVariable() {
+    }
+    /**
+     * Hanoi city Id.
+     */
     public static final int CITY_HANOI = 22;
+    /**
+     * Ho Chi Minh city Id.
+     */
     public static final int CITY_TPHCM = 29;
+    /**
+     * Da Nang city Id.
+     */
     public static final int CITY_DANANG = 15;
+    /**
+     * Nha Trang city Id.
+     */
     public static final int CITY_NHATRANG = 68;
+    /**
+     * Hai Phong city Id.
+     */
     public static final int CITY_HAIPHONG = 26;
+    /**
+     * Vung Tau city Id.
+     */
     public static final int CITY_VUNGTAU = 67;
+    /**
+     * Can Tho city Id.
+     */
     public static final int CITY_CANTHO = 14;
+    /**
+     * VND.
+     */
     public static final String VND = "VNĐ";
+    /**
+     * Non breaking space.
+     */
     public static final String NON_BREAKING_SPACE = "&nbsp;";
 }
