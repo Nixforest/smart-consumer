c9f21c3f8084620cdcac57d3813354857318a095
Nixforest21991920@gmail.com <Nixforest21991920@gmail.com@72819721-2bc9-f2f2-fa2e-d6c76ee130ff>
2012-10-31 15:19:14 +0000

    Update comment in ChangeStatusDone.java

--- SmartConsumer/src/com/gae/java/smartconsumer/action/admin/ChangeStatusDone.java
+++ SmartConsumer/src/com/gae/java/smartconsumer/action/admin/ChangeStatusDone.java
@@ -1,3 +1,8 @@
+/**
+ * ChangeStatusDone.java
+ * 28/5/2012
+ * Smart Consumer project
+ */
 package com.gae.java.smartconsumer.action.admin;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,19 +15,19 @@ import org.apache.struts.action.ActionMapping;
 
 import com.gae.java.smartconsumer.blo.DealBLO;
 import com.gae.java.smartconsumer.form.StatusForm;
-
+/**
+ * ChangeStatusDone action.
+ * @version 1.0 03/06/2012 - Create - NguyenPT
+ * @author NguyenPT
+ */
 public class ChangeStatusDone extends Action {
     @Override
     public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
             HttpServletResponse response) throws Exception {
         if (request.getParameter("url") == null) {
-            StatusForm status = (StatusForm)form;
-            //System.out.println(status.getId());
+            StatusForm status = (StatusForm) form;
             DealBLO.INSTANCE.changeStatus(status.getId(), status.getStatus());
-        } else {
-            String url = request.getParameter("url");
         }
-
         return mapping.findForward("success");
     }
 }
