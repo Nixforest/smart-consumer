005caac7334eea33cd930527268e36f8cd6c7ae1
Nixforest21991920@gmail.com <Nixforest21991920@gmail.com@72819721-2bc9-f2f2-fa2e-d6c76ee130ff>
2012-10-31 16:33:09 +0000

    Update comment

--- trunk/SmartConsumer/src/com/gae/java/smartconsumer/servlet/TaskQueueServlet.java
+++ trunk/SmartConsumer/src/com/gae/java/smartconsumer/servlet/TaskQueueServlet.java
@@ -1,8 +1,6 @@
 /**
  * TaskQueue.java
- * 
- * 28/5/2012
- * 
+ * 28/05/2012
  * Smart Consumer project
  */
 package com.gae.java.smartconsumer.servlet;
@@ -16,20 +14,17 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
 import com.google.appengine.api.taskqueue.TaskOptions.Method;
-
-import static com.google.appengine.api.taskqueue.TaskOptions.Builder.*;
-
+import static com.google.appengine.api.taskqueue.TaskOptions.Builder.withUrl;
 
 /**
- * Controller taskqueue
- * 
- * @version 1.0 28/5/2012
- * @author Nixforest
+ * Controller taskqueue.
+ * @version 1.0 28/05/2012
+ * @author NguyenPT
  */
 public class TaskQueueServlet extends HttpServlet {
     /**  . */
     private static final long serialVersionUID = 7769838809830051533L;
-
+    @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
         throws IOException {
         resp.setContentType("text/html");
